b6ad34e96e552b3c6e644386d9e07abb373c599d
lizechous <lizziepastenes@gmail.com>
2021-05-20 20:34:13 -0500

    correccion

--- apps/Medico/models.py
+++ apps/Medico/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+TIPO_ESPECIALIDAD = (
+    ('Cirugía', 'Cirugía'),
+    ('Cardiología', 'Cardiología'),
+    ('Odontología', 'Odontología'),
+    ('Pediatría', 'Pediatría'),
+    ('Ginecología', 'Ginecología'),
+    ('Oftomología', 'Oftomología'),
+)
+
+
+class Medico(models.Model):
+    autor_id = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    nombre_medico = models.TextField()
+    tipo_Especialidad = models.CharField(max_length=50, choices=TIPO_ESPECIALIDAD)
+
+
+    def __str__(self):
+        return self.nombre_medico
+    
--- clinicaalamos2/settings.py
+++ clinicaalamos2/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+from pathlib import Path
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.Medico',
 ]
 
 MIDDLEWARE = [
