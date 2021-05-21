11bbc9c378640e3c92625889157104990a20f927
MF Munoz <ma.munozleppez@gmail.com>
2021-05-21 19:42:35 -0400

    continuacion desarrollo de ingreso de horas

--- apps/Medico/forms.py
+++ apps/Medico/forms.py
@@ -4,23 +4,30 @@ from .models import CitaMedica
 class CitaMedicaForm(forms.ModelForm): 
     class Meta:  
         model = CitaMedica
-        fields = ['run', 'nombre_paciente', 'tipo_prevision', 'medico', 'especialidad', 'fecha_cita', 'hora_cita'] 
+        fields = ['run', 'nombre_paciente', 'medico', 'especialidad', 'fecha_cita', 'hora_cita'
+                  , 'email', 'precio', 'tipo_prevision'] 
 
         labels = {
             'run': 'Run',
             'nombre_paciente': 'Nombre paciente',
-            'tipo_prevision': 'Prevision', 
+             
             'medico': 'Nombre medico',
             'especialidad': 'Especialidad',
             'fecha_cita': 'Fecha Cita',
             'hora_cita': 'Hora Cita',
+            'email' : 'Email',
+            'precio' : 'Precio',
+            'tipo_prevision': 'Prevision',
         }
         widgets = {
             'run': forms.TextInput(attrs={'class': 'form-control'}), 
             'nombre_paciente': forms.TextInput(attrs={'class': 'form-control'}),
-            'tipo_prevision': forms.Select(choices="TIPO_PREVISION", attrs={'class':'form-control'}),
+            
             'medico': forms.TextInput(attrs={'class': 'form-control'}),
             'especialidad': forms.Select(choices="TIPO_ESPECIALIDAD", attrs={'class':'form-control'}),
             'fecha_cita' : forms.TextInput(attrs={'class': 'form-control'}),
             'hora_cita' : forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.TextInput(attrs={'class': 'form-control'}),
+            'precio' : forms.TextInput(attrs={'class': 'form-control'}),
+            'tipo_prevision': forms.Select(choices="TIPO_PREVISION", attrs={'class':'form-control'}),
         }
