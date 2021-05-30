58845523e95dbdbb2b471a9bd400a508da79d276
lizechous <lizziepastenes@gmail.com>
2021-05-29 23:49:53 -0400

    cambios nuevos

--- clinicaalamos2/urls.py
+++ clinicaalamos2/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import views as auth_views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.Medico.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
 
