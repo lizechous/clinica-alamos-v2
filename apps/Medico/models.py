12f2887969983913293e5535c559966940b76f27
lizechous <lizziepastenes@gmail.com>
2021-05-29 23:38:44 -0400

    subida codigo

--- apps/Medico/models.py
+++ apps/Medico/models.py
@@ -12,7 +12,8 @@ TIPO_ESPECIALIDAD = (
 
 
 class Medico(models.Model):
-    nombre_medico = models.TextField(max_length=30)
+    nombre_medico = models.TextField()
+    # nombre_medico = models.TextField(max_length=30)
     tipo_Especialidad = models.CharField(max_length=50, choices=TIPO_ESPECIALIDAD)
 
 
@@ -28,13 +29,13 @@ TIPO_PREVISION = (
 
 
 class CitaMedica(models.Model):
-    run = models.IntegerField(primary_key=True)
+    run= models.IntegerField(primary_key=True)
     nombre_paciente = models.TextField()
     tipo_prevision = models.CharField(max_length=50, choices=TIPO_PREVISION)
-    medico = models.ForeignKey(Medico, on_delete=models.CASCADE)
+    nombre_medico = models.ForeignKey(Medico, null=True, blank=True, on_delete=models.CASCADE)
     especialidad = models.CharField(max_length=50, choices=TIPO_ESPECIALIDAD)
     fecha_cita =  models.DateField()
-    hora_cita = models.TimeField(blank=True)
+    hora_cita = models.TimeField(blank=True, null=True)
     email = models.EmailField(max_length=70,blank=True, null= True, unique= True)
     precio = models.IntegerField()
 
