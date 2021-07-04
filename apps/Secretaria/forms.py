43199b42908ef75488092e29e2f489eb45b5a136
lizechous <lizziepastenes@gmail.com>
2021-07-04 13:31:06 -0400

    correccion rut y precarga de datos

--- apps/Secretaria/forms.py
+++ apps/Secretaria/forms.py
@@ -3,26 +3,25 @@ from .models import CitaMedica
 class CitaMedicaForm(forms.ModelForm): 
     class Meta:  
         model = CitaMedica
-        fields = ['run', 'nombre_paciente','tipo_prevision', 'especialidad', 'medico', 'fecha_cita', 'hora_cita'
-                  , 'email'] 
+        fields = ['run', 'nombre_paciente', 'email','tipo_prevision', 'especialidad', 'medico', 'fecha_cita', 'hora_cita'] 
 
         labels = {
             'run': 'Run',
             'nombre_paciente': 'Nombre paciente',
+            'email' : 'Email',
             'tipo_prevision': 'Prevision',
             'especialidad': 'Especialidad',
             'medico': 'Nombre medico',
             'fecha_cita': 'Fecha Cita',
             'hora_cita': 'Hora Cita',
-            'email' : 'Email',
         }
         widgets = {
-            'run': forms.TextInput(attrs={'class': 'form-control', "onKeyPress": "return soloTeclasRut(event);", "maxlength": "12"}), 
+            'run': forms.TextInput(attrs={'class': 'form-control', "onKeyPress": "return soloTeclasRut(event);", "maxlength": "9", "minlength": "7"}), 
             'nombre_paciente': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.TextInput(attrs={'class': 'form-control'}), 
             'tipo_prevision': forms.Select(attrs={'class':'form-control'}),
             'especialidad': forms.Select(attrs={'class':'form-control'}),
             'medico': forms.Select(attrs={'class':'form-control', 'disabled' : 'disabled'}),
             'fecha_cita' : forms.TextInput(attrs={'class': 'form-control', 'readonly' : 'readonly'}),
             'hora_cita' : forms.TextInput(attrs={'class': 'timepicker'}),
-            'email': forms.TextInput(attrs={'class': 'form-control'}), 
         }
